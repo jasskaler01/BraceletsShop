0be7fb097df54dd720f28945c43ee734de0c6160
jasskaler01 <W0782371@myscc.ca>
2022-02-24 23:32:09 -0500

    update entity frame work

---
+++ BraceletsShop/Models/Bracelet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BraceletsShop.Models
+{
+    public class Bracelet
+    {
+        public int ID { get; set; }
+        public string Category { get; set; }
+
+        public string Material { get; set; }
+
+        public DateTime  CollectionDate { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal Rating { get; set; }
+
+    }
+}
